a2a9f49912ec1fbe1381a845336cc684cf0950e6
ShravanJP <shravanjp8910@gmail.com>
2022-05-04 22:46:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/connect3/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.connect3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
